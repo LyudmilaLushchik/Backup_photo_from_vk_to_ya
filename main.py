b15f6aea6382140745a4f547f7e135183bc22d4a
LyudmilaLushchik <Lyudmila_c@rambler.ru>
2022-06-23 19:03:37 +0300

    Tokens are transfered to the external file settings.ini. PEP8 corrections made. Function log_report() created and transfered from Ya class.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import vk
-import ya
-from typing import Callable, Iterator, Union, Optional
+import vkontakte
+import yandex
+import configparser
+import json
 
 
 def copy_photo(photo_source, destination):
@@ -10,22 +11,33 @@ def copy_photo(photo_source, destination):
     '''
     user_id_status = False
     while not user_id_status:
-        user_id_status = photo_source.get_user_id(input('Введите \
-короткое имя(screen_name) или id пользователя ВКонтакте: '))
+        user_id_status = photo_source.get_user_id(input('Введите '
+'короткое имя(screen_name) или id пользователя ВКонтакте: '))
+
     photo_source.get_albums()
-    photo_source.select_album(input(f'Для резервного копирования фото из альбома \
-введите соответствующее по списку число(по умолчанию фотографии профиля): '))
+    photo_source.select_album(input(f'Для резервного копирования фото из альбома '
+'введите соответствующее по списку число(по умолчанию фотографии профиля): '))
     photos = photo_source.get_photos(input('Введите количество фотографий(по умолчанию 5): '))
 
     dir_ya_status = False
     while not dir_ya_status:
-        dir_ya_status = destination.create_new_folder(input('Введите \
-название папки на Яндекс.Диске(по умолчанию photos_from_vk): '))
-    destination.upload_photo_to_disk(photos)
+        dir_ya_status = destination.create_new_folder(input('Введите '
+'название папки на Яндекс.Диске(по умолчанию photos_from_vk): '))
+    return destination.upload_photo_to_disk(photos)
+
+def log_report(files_uploaded):
+        with open('results.json', 'w') as res_file:
+            json.dump(files_uploaded, res_file)
 
 if __name__ == '__main__':
-    TOKEN_VK = '_'
-    TOKEN_YA = '_'
-    vk_client = vk.Vk(TOKEN_VK)
-    ya_client = ya.Ya(TOKEN_YA)
-    copy_photo(vk_client, ya_client)
+    config = configparser.ConfigParser()
+    config.read("settings.ini")
+    token_vk = config["vkontakte"]["token"]
+    token_ya = config["yandex"]["token"]
+    default_vk = {}
+    default_vk['photo_count'] = config['default_vk']['def_photo_count']
+    default_vk['album'] = config['default_vk']['def_album']
+    default_dir_ya = config['default_ya']['def_dir']
+    vk_client = vkontakte.Vk(token_vk, **default_vk)
+    ya_client = yandex.Ya(token_ya, default_dir_ya)
+    log_report(copy_photo(vk_client, ya_client))
